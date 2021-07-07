1ee214ad268d8e16b0049de452dee36ab94db803
whitehead12 <dwhitehead1206@gmail.com>
2021-07-07 18:13:48 +0000

    add portfolio.html file

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -27,8 +27,10 @@
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
         
         // example: 
-        createCannon("top", 450);
-        
+        createCannon("top", 450, 3000);
+        createCannon("right",380, 2000);
+        createCannon("left", 100, 9000);
+
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
